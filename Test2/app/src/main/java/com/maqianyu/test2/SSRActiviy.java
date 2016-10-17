09730d8250c3eba252c3a61bc8fdb22b841e247b
ZhangHongdiL <1205304542@qq.com>
2016-10-17 14:44:24 +0800

    Picasso加载图片

---
+++ Test2/app/src/main/java/com/maqianyu/test2/SSRActiviy.java
@@ -0,0 +1,16 @@
+package com.maqianyu.test2;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by dllo on 16/10/17.
+ */
+public class SSRActiviy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
