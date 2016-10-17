6dbb5e3bd1c390c26df06af4a2eb88d792fcc8fa
qiansha <pangmeilnyx@163.com>
2016-10-17 15:01:32 +0800

    创建Fragment

---
+++ Test2/app/src/main/java/com/maqianyu/test2/OneFragment.java
@@ -0,0 +1,19 @@
+package com.maqianyu.test2;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by dllo on 16/10/17.
+ */
+public class OneFragment extends Fragment{
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
